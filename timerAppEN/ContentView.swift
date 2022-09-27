6c46fc3a69f3ce5f6a083bf2970baa8752810462
Tui Alailima <82783671+coachtui@users.noreply.github.com>
2022-09-26 15:55:54 -1000

    Add files via upload

---
+++ timerAppEN/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  timerAppEN
+//
+//  Created by Tuisamau Alailima on 9/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var timerManager = TimerManager()
+    
+    @State var selectedPickerIndex = 0
+    
+    let availableMinutes = Array(1...180)
+    
+    var body: some View {
+        
+        NavigationView{
+            
+            VStack {
+                Text(secondsToMinutesAndSeconds(seconds: timerManager.secondsLeft))
+                    .font(.system(size: 80))
+                    .padding(.top, 80)
+                Image(systemName: timerManager.timerMode == .running ? "pause.circle.fill" : "play.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                    .foregroundColor(.red)
+                    .onTapGesture(perform:{
+                        if self.timerManager.timerMode == .initial {
+                            self.timerManager.setTimerLength(minutes: self.availableMinutes[self.selectedPickerIndex]*60)
+                        }
+                        self.timerManager.timerMode == .running ? self.timerManager.pause() : self.timerManager.start()
+                    })
+                if timerManager.timerMode == .paused {
+                    Image(systemName: "gobackward")
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .padding(.top, 40)
+                        .onTapGesture(perform:{
+                            self.timerManager.reset()
+                        })
+                }
+                if timerManager.timerMode == .initial{
+                    Picker(selection: $selectedPickerIndex, label: Text("")){
+                        ForEach(0 ..< availableMinutes.count){
+                            Text("\(self.availableMinutes[$0]) min")
+                        }
+                    }
+                    .labelsHidden()
+                }
+                Spacer()
+            }
+            .navigationTitle("Timer")
+        }
+    }
+}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
---
+++ timerAppEN/timerAppENApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerAppENApp.swift
+//  timerAppEN
+//
+//  Created by Tuisamau Alailima on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct timerAppENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
